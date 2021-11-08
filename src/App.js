f62393ee8898b00adc96f6cfb10de04641372c52
PedroLoebens <pedro.loebens@acertapromotora.com>
2021-11-08 20:25:15 -0300

    O primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios';
 
 function App() {
+  const [packageData, setPackageData] = useState();
+  const [input, setInput] = useState({ code: '', type: 'LB' });
+
+  const search = () => {
+    axios.post('https://correios.contrateumdev.com.br/api/rastreio', input)
+      .then((response) => setPackageData(response.data))
+  }
+
+  const handleChange = (event) => {
+
+    const newCode = event.target.value;
+    setInput((prevState) => ({ ...prevState, code: newCode }));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={input.code} onChange={(event) => handleChange(event)} />
+
+      <button onClick={() => search()}>Buscar</button>
+      <div>
+        {packageData && packageData.quantidade}
+      </div>
+      <div> 
+        {packageData && packageData.objeto[0].evento.map((obj) => (obj.descricao))}
+      </div>
+
     </div>
   );
 }
